51ca971daf50aaab4f64d1fa7578b1e760bb861d
Azat Azmetov <azmetoff@gmail.com>
2021-11-15 22:37:28 +0300

    db abctract classes and interfaces created

--- app/src/main/java/com/example/mycryptoapp/pojo/CoinInfo.kt
+++ app/src/main/java/com/example/mycryptoapp/pojo/CoinInfo.kt
@@ -4,19 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class CoinInfo(
-    @SerializedName("Id")
-    @Expose
-    var id: String? = null,
-
     @SerializedName("Name")
     @Expose
     var name: String? = null,
-
-    @SerializedName("FullName")
-    @Expose
-    var fullName: String? = null,
-
-    @SerializedName("ImageUrl")
-    @Expose
-    var imageUrl: String? = null
 )
--- app/src/main/java/com/example/mycryptoapp/pojo/CoinPriceInfo.kt
+++ app/src/main/java/com/example/mycryptoapp/pojo/CoinPriceInfo.kt
@@ -1,10 +1,12 @@
 package com.example.mycryptoapp.pojo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 
 import com.google.gson.annotations.SerializedName
 
-
+@Entity(tableName = "full_price_list")
 data class CoinPriceInfo(
     @SerializedName("TYPE")
     @Expose
@@ -14,6 +16,7 @@ data class CoinPriceInfo(
     @Expose
     var market: String? = null,
 
+    @PrimaryKey
     @SerializedName("FROMSYMBOL")
     @Expose
     var fromsymbol: String? = null,
