e25b23438a2efb18ee25cabe07c0b56763376dd2
Azat Azmetov <azmetoff@gmail.com>
2022-01-24 16:29:11 +0300

    added CoinDetailFragment.kt and used it in Activity

--- app/src/main/java/com/example/mycryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/example/mycryptoapp/presentation/CoinDetailActivity.kt
@@ -4,17 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.mycryptoapp.R
 import com.example.mycryptoapp.databinding.ActivityCoinDetailBinding
-import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityCoinDetailBinding.inflate(layoutInflater)
     }
-    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,18 +22,11 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this) {
-            with(binding) {
-                tvPrice.text = it.price.toString()
-                tvMinPrice.text = it.lowday.toString()
-                tvMaxPrice.text = it.highday.toString()
-                tvLastMarket.text = it.lastmarket.toString()
-                tvLastUpdate.text = it.lastupdate
-                tvFromSymbol.text = it.fromsymbol
-                tvToSymbol.text = it.tosymbol
-                Picasso.get().load(it.imageurl).into(ivLogoCoin)
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
 
---
+++ app/src/main/java/com/example/mycryptoapp/presentation/CoinDetailFragment.kt
@@ -0,0 +1,65 @@
+package com.example.mycryptoapp.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.mycryptoapp.databinding.FragmentCoinDetailBinding
+import com.squareup.picasso.Picasso
+
+class CoinDetailFragment : Fragment() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    private var _binding: FragmentCoinDetailBinding? = null
+    private val binding: FragmentCoinDetailBinding
+        get() = _binding ?: throw RuntimeException("FragmentCoinDetailBinding is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCoinDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fromSymbol = getSymbol()
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.getDetailInfo(fromSymbol).observe(viewLifecycleOwner) {
+            with(binding) {
+                tvPrice.text = it.price.toString()
+                tvMinPrice.text = it.lowday.toString()
+                tvMaxPrice.text = it.highday.toString()
+                tvLastMarket.text = it.lastmarket.toString()
+                tvLastUpdate.text = it.lastupdate
+                tvFromSymbol.text = it.fromsymbol
+                tvToSymbol.text = it.tosymbol
+                Picasso.get().load(it.imageurl).into(ivLogoCoin)
+            }
+        }
+    }
+
+    private fun getSymbol(): String {
+        return requireArguments().getString(EXTRA_FROM_SYMBOL, EMPTY_SYMBOL)
+    }
+
+    companion object {
+        private const val EXTRA_FROM_SYMBOL = "fSym"
+        private const val EMPTY_SYMBOL = ""
+
+        fun newInstance(fromSymbol: String): Fragment {
+            return CoinDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(EXTRA_FROM_SYMBOL, fromSymbol)
+                }
+            }
+        }
+    }
+}
